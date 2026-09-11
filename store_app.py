e6ad4fcb8620cc2c917a97622ea28b7840a82eda
cscapsatyam <cscapsatyam@gmail.com>
2026-09-11 15:52:56 +0530

    Update store_app.py

--- store_app.py
+++ store_app.py
@@ -661,5 +661,5 @@ elif page == "9. AKG Shutterings Ledger":
 
 
 # ================= PAGE 10: DAILY SITE & MANPOWER REPORT =================
-elif page == "10. Movone Infrastructure Private Limited "
+elif page == "10. Movone Infrastructure Private Limited ":
   page10.run()
